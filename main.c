8e9d4f84e4ffb8b501c3ad151b7dd2b1fe18fb44
douglasselias <douglassantoselias@gmail.com>
2021-03-02 14:28:56 -0300

    Add glew

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+// #define GLEW_STATIC
+// #include <GL/glew.h>
 
 typedef struct
 {
@@ -13,6 +17,19 @@ typedef Vertex Triangle[3];
 
 int main()
 {
+  // glewExperimental = GL_TRUE;
+  // glewInit();
+
+  // GLuint vertexBuffer;
+  // glGenBuffers(1, &vertexBuffer);
+
+  // printf("%u\n", vertexBuffer);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
   Vertex v = {
       1.3,
       1.9,
@@ -26,8 +43,26 @@ int main()
 
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  SDL_Delay(1000);
+  SDL_Window *window = SDL_CreateWindow("Hello SDL!", 200, 200, 900, 500, SDL_WINDOW_MAXIMIZED);
+  SDL_GLContext *context = SDL_GL_CreateContext(window);
 
+  // SDL_Delay(5000);
+  // if (windowEvent.type == SDL_KEYUP &&
+  // windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+  SDL_Event event;
+
+  while (true)
+  {
+    if (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT)
+        break;
+    }
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(context);
   SDL_Quit();
 
   return 0;
--- makefile
+++ makefile
@@ -1,7 +1,13 @@
 FILE=main
 
+sdlFlags=`sdl2-config --cflags --libs`
+glewFlags=-DGLEW_STATIC -lGLEW -lGLU -lGL
+
 make:
-	gcc ${FILE}.c -o ${FILE} `sdl2-config --cflags --libs`
+	gcc ${FILE}.c -o ${FILE} $(sdlFlags) -Werror
+
+run:
+	clear && ./main
 
 print: $(wildcard *.c)
 	ls -1 $?
